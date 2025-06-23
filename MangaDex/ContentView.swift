a59aaa349f1870e9fc4e33f1b4843b445169fc41
Aitor Ballesteros <aitor.ballesteros@appspace.com>
2025-06-23 15:58:23 +0200

    Initial Commit

---
+++ MangaDex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MangaDex
+//
+//  Created by APPSPACE on 23/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MangaDex/MangaDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaDexApp.swift
+//  MangaDex
+//
+//  Created by APPSPACE on 23/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MangaDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
